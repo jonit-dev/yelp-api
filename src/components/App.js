0bc4cb71f91e496f12ad79d51497779443ca981e
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2019-04-05 21:40:24 -0700

    First commit, setting up dependencies

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+   // state = {key:value};
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default App;
